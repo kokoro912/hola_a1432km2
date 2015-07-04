b89efd37b258ca0a80a832bb2a5805e8751c2c39
vagrant <vagrant@enpit>
2015-07-04 12:02:16 +0000

    add prime calc function

--- Gemfile
+++ Gemfile
@@ -1,4 +1,9 @@
 source 'https://rubygems.org'
 
+#素数ライブラリをインストール
+gem 'prime', '>=0.1'
+
+gem 'rspec', '~> 0'
+
 # Specify your gem's dependencies in hola_a1432km2.gemspec
 gemspec
--- lib/hola_a1432km2.rb
+++ lib/hola_a1432km2.rb
@@ -1,6 +1,9 @@
 require "hola_a1432km2/version"
 
 module HolaA1432km2
+	require 'time'
+	require 'prime'
+	
 	#誕生日を元に現在の歳を出力
 	def self.calcYourAge(date_str)
 		#現在日
@@ -13,15 +16,21 @@ module HolaA1432km2
 		your_age = sprintf("%d",((today - birthday) / 60 / 60 / 24)/365)
 
 		#出力
-		p your_age
+		puts "Your age is #{your_age} now."
 	end
 
 	#身長と体重を元にBMIを出力
 	def self.calcYourBMI(height, weight)
 		#BMIを計算
-		bmi = sprintf( "%10.2f", weight / ( height * height) )
+		your_bmi = sprintf( "%10.2f", weight / ( height * height) )
 		
 		#出力
-		p bmi
+		puts "Your BMI is #{your_bmi}"
+	end
+	
+	#指定した数字までの素数を出力する
+	def self.calcPrimeNumber(count)
+		puts Prime.take(count)
+		puts "#{count} prime numbers are listed."
 	end
 end
